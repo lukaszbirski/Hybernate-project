b4c2609432d0a1d0fb0d863cbc62608b2d169d52
lukaszbirski <lukasz.birski@gmail.com>
2018-10-13 15:43:33 +0200

    first commit

---
+++ src/main/java/com/example/HibernateProject/HibernateProjectApplication.java
@@ -0,0 +1,12 @@
+package com.example.HibernateProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateProjectApplication.class, args);
+	}
+}
